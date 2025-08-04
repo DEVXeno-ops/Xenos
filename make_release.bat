d1271e2a1d45239728904dc103c5f32fb7e903db
Mill10m <steastea77@gmail.com>
2025-08-04 23:05:06 +0700

    Update make_release.bat

--- make_release.bat
+++ make_release.bat
@@ -1,27 +1,44 @@
 @echo off
+setlocal
 
+:: Check for version argument
 if "%~1"=="" (
-	echo Release version must be specified as an argument
-	goto quit
+    echo [ERROR] Release version must be specified as an argument.
+    goto :quit
 )
 
-set ver=%1
-set zipper="C:\Program Files\7-Zip\7z.exe"
+set "ver=%~1"
+set "zipper=C:\Program Files\7-Zip\7z.exe"
 
-mkdir %ver%
+:: Check if 7-Zip exists
+if not exist "%zipper%" (
+    echo [ERROR] 7-Zip not found at "%zipper%". Please install 7-Zip or update the path.
+    goto :quit
+)
 
-cd %ver%
-copy ..\build\Win32\Release\Xenos.exe Xenos.exe
-copy ..\build\x64\Release\Xenos64.exe Xenos64.exe
-copy ..\Releases\Changelog.txt Changelog.txt
-copy ..\Releases\Readme.txt Readme.txt
-%zipper% a -t7z Xenos_%ver%.7z * -mx=9 -myx=9
-copy Xenos_%ver%.7z ..\Releases\Xenos_%ver%.7z
-cd ..
-rmdir /S /Q %ver%
+:: Create temp release folder
+mkdir "%ver%" 2>nul
 
-mkdir Releases\Pdb\%ver%
-copy  build\Win32\Release\Xenos.pdb Releases\Pdb\%ver%\Xenos.pdb
-copy  build\x64\Release\Xenos64.pdb Releases\Pdb\%ver%\Xenos64.pdb
+pushd "%ver%"
+copy "..\build\Win32\Release\Xenos.exe" >nul
+copy "..\build\x64\Release\Xenos64.exe" >nul
+copy "..\Releases\Changelog.txt" >nul
+copy "..\Releases\Readme.txt" >nul
+
+:: Compress using 7-Zip
+"%zipper%" a -t7z "Xenos_%ver%.7z" * -mx=9 -myx=9 >nul
+
+:: Move archive to Releases
+copy "Xenos_%ver%.7z" "..\Releases\Xenos_%ver%.7z" >nul
+popd
+
+:: Clean up temp folder
+rmdir /S /Q "%ver%"
+
+:: Copy PDB files
+mkdir "Releases\Pdb\%ver%" 2>nul
+copy "build\Win32\Release\Xenos.pdb" "Releases\Pdb\%ver%\Xenos.pdb" >nul
+copy "build\x64\Release\Xenos64.pdb" "Releases\Pdb\%ver%\Xenos64.pdb" >nul
 
 :quit
+endlocal
