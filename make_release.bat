d44c9331cc183e16f4f18436c4b0bb45542bf433
Mill10m <steastea77@gmail.com>
2025-08-08 17:01:17 +0700

    Update make_release.bat

--- make_release.bat
+++ make_release.bat
@@ -1,44 +1,89 @@
 @echo off
-setlocal
+setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-:: Check for version argument
+:: =========================
+::   Release Packaging Script
+:: =========================
+
+:: Root path = folder ที่ไฟล์ .bat อยู่
+set "ROOT=%~dp0"
+
+:: ===== Check version argument =====
 if "%~1"=="" (
     echo [ERROR] Release version must be specified as an argument.
+    echo Usage: %~nx0 ^<version^>
     goto :quit
 )
 
 set "ver=%~1"
 set "zipper=C:\Program Files\7-Zip\7z.exe"
 
-:: Check if 7-Zip exists
+:: ===== Check if 7-Zip exists =====
 if not exist "%zipper%" (
-    echo [ERROR] 7-Zip not found at "%zipper%". Please install 7-Zip or update the path.
+    echo [ERROR] 7-Zip not found at "%zipper%".
+    echo Please install 7-Zip or update the path in the script.
     goto :quit
 )
 
-:: Create temp release folder
-mkdir "%ver%" 2>nul
+:: ===== Create temp release folder =====
+set "TMPDIR=%ROOT%%ver%"
+if exist "%TMPDIR%" rmdir /S /Q "%TMPDIR%"
+mkdir "%TMPDIR%" 2>nul
 
-pushd "%ver%"
-copy "..\build\Win32\Release\Xenos.exe" >nul
-copy "..\build\x64\Release\Xenos64.exe" >nul
-copy "..\Releases\Changelog.txt" >nul
-copy "..\Releases\Readme.txt" >nul
+:: ===== Copy release files =====
+echo [INFO] Copying release files...
+if exist "%ROOT%build\Win32\Release\Xenos.exe" (
+    robocopy "%ROOT%build\Win32\Release" "%TMPDIR%" "Xenos.exe" /NFL /NDL /NJH /NJS /NC /NS >nul
+) else (
+    echo [WARN] Win32 Release build not found.
+)
 
-:: Compress using 7-Zip
+if exist "%ROOT%build\x64\Release\Xenos64.exe" (
+    robocopy "%ROOT%build\x64\Release" "%TMPDIR%" "Xenos64.exe" /NFL /NDL /NJH /NJS /NC /NS >nul
+) else (
+    echo [WARN] x64 Release build not found.
+)
+
+for %%F in (Changelog.txt Readme.txt) do (
+    if exist "%ROOT%Releases\%%F" (
+        robocopy "%ROOT%Releases" "%TMPDIR%" "%%F" /NFL /NDL /NJH /NJS /NC /NS >nul
+    ) else (
+        echo [WARN] %%F not found in Releases folder.
+    )
+)
+
+:: ===== Compress using 7-Zip =====
+pushd "%TMPDIR%"
+echo [INFO] Creating archive Xenos_%ver%.7z ...
 "%zipper%" a -t7z "Xenos_%ver%.7z" * -mx=9 -myx=9 >nul
-
-:: Move archive to Releases
-copy "Xenos_%ver%.7z" "..\Releases\Xenos_%ver%.7z" >nul
 popd
 
-:: Clean up temp folder
-rmdir /S /Q "%ver%"
+:: ===== Move archive to Releases =====
+if not exist "%ROOT%Releases" mkdir "%ROOT%Releases"
+move "%TMPDIR%\Xenos_%ver%.7z" "%ROOT%Releases\Xenos_%ver%.7z" >nul
 
-:: Copy PDB files
-mkdir "Releases\Pdb\%ver%" 2>nul
-copy "build\Win32\Release\Xenos.pdb" "Releases\Pdb\%ver%\Xenos.pdb" >nul
-copy "build\x64\Release\Xenos64.pdb" "Releases\Pdb\%ver%\Xenos64.pdb" >nul
+:: ===== Clean up temp folder =====
+rmdir /S /Q "%TMPDIR%"
+
+:: ===== Copy PDB files =====
+echo [INFO] Copying PDB files...
+set "PDBDIR=%ROOT%Releases\Pdb\%ver%"
+if not exist "%PDBDIR%" mkdir "%PDBDIR%"
+
+if exist "%ROOT%build\Win32\Release\Xenos.pdb" (
+    copy "%ROOT%build\Win32\Release\Xenos.pdb" "%PDBDIR%\Xenos.pdb" >nul
+) else (
+    echo [WARN] Win32 PDB not found.
+)
+
+if exist "%ROOT%build\x64\Release\Xenos64.pdb" (
+    copy "%ROOT%build\x64\Release\Xenos64.pdb" "%PDBDIR%\Xenos64.pdb" >nul
+) else (
+    echo [WARN] x64 PDB not found.
+)
+
+echo [INFO] Packaging complete: %ROOT%Releases\Xenos_%ver%.7z
+goto :eof
 
 :quit
 endlocal
